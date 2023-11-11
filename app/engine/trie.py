bc65f8f40086e9b0f3a689750297dd6b1d5c545d
author31 <authorwong31@gmail.com>
2023-11-12 05:32:19 +0700

    [Code] added build trie with longer words

--- app/engine/trie.py
+++ app/engine/trie.py
@@ -24,10 +24,32 @@ class Trie:
     def char_to_idx(self, char: str) -> int:
         return ord(char) - ord("a")
 
-    def build_trie(self, vocabs: List) -> None:
-        for word in vocabs: self.insert_trie(word)
+    def build_word_trie(self, vocabs: List) -> None:
+        for word in vocabs: self.insert_word_to_trie(word)
 
-    def insert_trie(self, word):
+    def build_sentence_trie(self, vocabs: List) -> None:
+        for word in vocabs: self.insert_sentence_to_trie(word)
+
+    def insert_sentence_to_trie(self, sentence: str) -> None:
+        words = sentence.split(" ")
+        temp_node = self.root_node
+        for idx in range(len(words)):
+            char_idx = self.char_to_idx(words[idx][0])
+
+            if not temp_node.children[char_idx]: 
+                temp_node.children[char_idx] = TrieNode(words[idx])
+
+            if words[idx] == temp_node.children[char_idx].char and idx == len(words)-1: 
+                temp_node.children[char_idx].freq += 1
+
+            if idx == len(words)-1:
+                temp_node.children[char_idx].is_end_of_word = True
+                return
+
+            temp_node = temp_node.children[char_idx]
+            
+
+    def insert_word_to_trie(self, word: str) -> None:
         temp_node = self.root_node
         for idx in range(len(word)):
             char_idx = self.char_to_idx(word[idx])
@@ -44,12 +66,28 @@ class Trie:
 
             temp_node = temp_node.children[char_idx]
             
-    def init_cache(self):
+    def init_cache(self) -> None:
         if not self.root_node.has_children(): raise Exception("Trie is empty")
         for node in self.root_node.children: 
             if not node: continue
             self._cache_topk(node)
             
+    def retrieve_sentences(self, prefix: str) -> List[str]:
+        char_idx = self.char_to_idx(prefix)
+        temp_node = self.root_node.children[char_idx]
+        if not temp_node: return []
+        return self._dfs(prefix=temp_node.char, node=temp_node)
+
+    def _dfs(self, prefix: str, node: TrieNode) -> List[str]:
+        sentences = []
+        if not node: return sentences
+        if node.is_end_of_word: sentences.append(prefix)
+        for n in node.children:
+            if not n: continue
+            sentences.extend(self._dfs(prefix=prefix+ " " +n.char, node=n))
+        return sentences
+
+
     def search(self, word: str) -> TrieNode:
         temp_node = self.root_node
         for idx in range(len(word)):
